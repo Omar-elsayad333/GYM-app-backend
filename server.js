2f4dcb7199844c9457c7d152a5222c92323af9d5
omar-elsayad <kklskull1234@gmail.com>
2023-02-20 06:22:57 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts.js')
+
+// Express app
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen to requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listeneing on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    }
+)
